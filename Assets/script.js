4d3be3070f336cfdc482d03e8fc5baa4064775ea
Oscar Medina <ogmedina04@gmail.com>
2020-11-13 16:03:04 -0600

    finished script, need to clean up CSS file

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,47 @@
+//Time thats in the Jumbotron
+var update = function(){
 var currentDay = moment().format("dddd MMMM do, h:mm:ss a");
     $("#currentDay").text(currentDay);
+}
+//checks every 1000 ms to reset seconds in time
+setInterval(update, 1000);
 
+//When the user clicks the save button, it gets the value of the description and the attribute to save the time
+$(".saveBtn").on("click", function() {
 
+    var timeSaved = $(this).parent().attr("id");
+    var textAgenda = $(this).siblings(".description").val();
+    
+    console.log(timeSaved, textAgenda);
 
+    localStorage.setItem(timeSaved, textAgenda);
 
+});
+//finds the current time with the moment.js library
+var currentHour = moment().hour(); 
+console.log(currentHour);
 
+//function to change the colors based on what time it is
+$(".description").each(function(){
+    var agendaHour = $(this).attr("id");
+
+    if (currentHour == agendaHour){
+        $(this).addClass("present");
+    }
+    else if (currentHour > agendaHour){
+        $(this).removeClass("present");
+        $(this).addClass("past");
+    }
+
+    else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+    }
+});
+
+//for loop to write the value of the class plus the number plus the description based on the number from getItem()
+for (let i=9; i <=17; i++){
+
+    $("#" + i + ".description").val(localStorage.getItem(i))
+}
